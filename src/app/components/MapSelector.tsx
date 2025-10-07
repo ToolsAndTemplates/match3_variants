bcce9c39d58f09ab17bb4b0f354518d8a0b60227
ismat-samadov <ismetsemedov@gmail.com>
2025-10-07 17:54:12 +0400

    init

--- src/app/components/MapSelector.tsx
+++ src/app/components/MapSelector.tsx
@@ -5,24 +5,10 @@ import 'leaflet/dist/leaflet.css'
 import { useEffect, useState } from 'react'
 import L from 'leaflet'
 
-// Fix for default icon issue with webpack
-import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png?url';
-import iconUrl from 'leaflet/dist/images/marker-icon.png?url';
-import shadowUrl from 'leaflet/dist/images/marker-shadow.png?url';
-
-const DefaultIcon = L.icon({
-    iconRetinaUrl,
-    iconUrl,
-    shadowUrl,
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    tooltipAnchor: [16, -28],
-    shadowSize: [41, 41]
-});
-
-L.Marker.prototype.options.icon = DefaultIcon;
-
+// image imports -- TypeScript will accept these if you added the declarations above
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png?url'
+import iconUrl from 'leaflet/dist/images/marker-icon.png?url'
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png?url'
 
 interface Location {
   latitude: number | null
@@ -37,11 +23,25 @@ interface MapSelectorProps {
 export default function MapSelector({ onLocationSelect }: MapSelectorProps) {
   const [locations, setLocations] = useState<Location[]>([])
 
+  // Set the default Leaflet icon URLs once (client-side)
+  // mergeOptions exists on L.Icon.Default and expects IconOptions
+  // cast to any/appropriate interface only if TypeScript complains about types
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: iconRetinaUrl as string,
+    iconUrl: iconUrl as string,
+    shadowUrl: shadowUrl as string
+  } as L.IconOptions)
+
   useEffect(() => {
     const fetchLocations = async () => {
-      const response = await fetch('/api/locations')
-      const data = await response.json()
-      setLocations(data)
+      try {
+        const response = await fetch('/api/locations')
+        if (!response.ok) throw new Error(`HTTP ${response.status}`)
+        const data = await response.json()
+        setLocations(data)
+      } catch (err) {
+        console.error('Failed to fetch locations', err)
+      }
     }
     fetchLocations()
   }, [])
@@ -52,17 +52,20 @@ export default function MapSelector({ onLocationSelect }: MapSelectorProps) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      {locations.map((location, index) => (
-        location.latitude && location.longitude && (
+      {locations.map((location, index) =>
+        location.latitude && location.longitude ? (
           <Marker key={index} position={[location.latitude, location.longitude]}>
             <Popup>
-              {location.name}
-              <br />
-              <button onClick={() => onLocationSelect(location.name || '')}>Select</button>
+              <div>
+                <div>{location.name}</div>
+                <button type="button" onClick={() => onLocationSelect(location.name ?? '')}>
+                  Select
+                </button>
+              </div>
             </Popup>
           </Marker>
-        )
-      ))}
+        ) : null
+      )}
     </MapContainer>
   )
 }
